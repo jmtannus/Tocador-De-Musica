9c57e18ff0aa74bdfecabccf026a5f0307963d7c
Janaina <jmtannus@gmail.com>
2023-09-08 01:14:49 -0300

    Acrescentando dados botão de like

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const cover = document.getElementById("cover");
 const play = document.getElementById("play");
 const next = document.getElementById("next");
 const previous = document.getElementById("previous");
+const likeButton = document.getElementById('like');
 const currentProgress = document.getElementById('current-progress');
 const progressContainer = document.getElementById('progress-container');
 const shuffleButton = document.getElementById('shuffle');
@@ -15,67 +16,79 @@ const totalTime = document.getElementById('total-time');
 const weaponOfChoice = {
   songName : "Weapon of Choice" ,
   artist : "Fatboy Slim" ,
-  file : "weapon-of-choice"
+  file : "weapon-of-choice",
+  liked: false,
 } ;
 const hangingByaMoment = {
   songName : "Hanging by a moment" ,
   artist : "Lifehouse" ,
-  file : "hanging-by-a-moment"
+  file : "hanging-by-a-moment",
+  liked: false,
 };
 const littleTalks = {
   songName : "Little Talks" ,
   artist : "Of Monsters and Men" ,
-  file : "littleTalks"
+  file : "littleTalks",
+  liked: false,
 };
 const lastNite = {
   songName : "Last Nite" ,
   artist : "Jumbonics" ,
-  file : "lastNite"
+  file : "lastNite",
+  liked: false,
 };
 const andreaDoria = {
   songName : "Andrea Doria" ,
   artist : "Legiao Urbana" ,
-  file : "andreaDoria"
+  file : "andreaDoria",
+  liked: false,
 } ;
 const hearMeOut = {
   songName : "Hear Me Out" ,
   artist : "Frou Frou" ,
-  file : "hearMeOut"
+  file : "hearMeOut",
+  liked: false,
 };
 const cmere = {
   songName : "C mere" ,
   artist : "Interpol" ,
-  file : "cmere"
+  file : "cmere",
+  liked: false,
 };
 const faint = {
   songName : "Faint" ,
   artist : "Linkin Park" ,
-  file : "faint"
+  file : "faint",
+  liked: false,
 };
 const wish = {
   songName : "I Wish I Knew How It Would Feel To Be Free" ,
   artist : "Billy Taylor" ,
-  file : "wish"
+  file : "wish",
+  liked: false,
 };
 const rockafellerSkank = {
   songName : "The Rockafeller Skank " ,
   artist : "Fatboy Slim" ,
-  file : "rockafellerSkank"
+  file : "rockafellerSkank",
+  liked: false,
 };
 const thatGirl = {
   songName : "That Girl" ,
   artist : "Maxi Priest & Shaggy" ,
-  file : "thatGirl"
+  file : "thatGirl",
+  liked: false,
 };
 const euSei = {
   songName : "Eu sei" ,
   artist : "Papas Na Lingua" ,
-  file : "euSei"
+  file : "euSei",
+  liked: false,
 };
 let isPlaying = false;
 let isShuffled = false;
 let repeatOn = false;
-const originalPlaylist = [weaponOfChoice, hangingByaMoment, littleTalks, lastNite, andreaDoria, hearMeOut, cmere, faint, wish, rockafellerSkank, thatGirl, euSei];
+const originalPlaylist = JSON.parse(localStorage.getItem('playlist'));
 let sortedPlaylist = [...originalPlaylist]
 let index = 0;
 
@@ -101,11 +114,24 @@ function playPauseDecider() {
   }
 }
 
+function likeButtonRender() {
+  if (sortedPlaylist[index].liked === true) {
+    likeButton.querySelector('.bi').classList.remove('bi-heart');
+    likeButton.querySelector('.bi').classList.add('bi-heart-fill');
+    likeButton.classList.add('button-active');
+  } else {
+    likeButton.querySelector('.bi').classList.add('bi-heart');
+    likeButton.querySelector('.bi').classList.remove('bi-heart-fill');
+    likeButton.classList.remove('button-active');
+  }
+}
+
 function initializeSong() {
   cover.src = `images/${sortedPlaylist[index].file}.jpg`;
   song.src = `songs/${sortedPlaylist[index].file}.mp3`;
   songName.innerText = sortedPlaylist[index].songName;
   bandName.innerText = sortedPlaylist[index].artist;
+  likeButtonRender() ;
 }
 
 function previousSong() {
@@ -206,6 +232,16 @@ function updateTotalTime() {
   songTime.innerText = song.duration;
 }
 
+function likeButtonClicked() {
+  if (sortedPlaylist[index].liked === false) {
+    sortedPlaylist[index].liked = true;
+  } else {
+    sortedPlaylist[index].liked = false;
+  }
+  likeButtonRender();
+  localStorage.setItem('playlist', JSON.stringify(originalPlaylist));
+}
+
 initializeSong();
 
 play.addEventListener("click", playPauseDecider);
@@ -217,3 +253,4 @@ song.addEventListener("loadedmetadata", updateTotalTime);
 progressContainer.addEventListener("click", jumpTo);
 shuffleButton.addEventListener("click", shuffleButtonClicked);
 repeatButton.addEventListener('click', repeatButtonClicked);
+likeButton.addEventListener('click', likeButtonClicked);
