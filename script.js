aca5a5307540aed346291e37fef81c2b1372af1d
JanainaMTTeixeira <61756665+jmtannus@users.noreply.github.com>
2023-09-12 13:53:47 +0000

    Extensão de pesquisa de imagem: webp --> jpg

--- script.js
+++ script.js
@@ -140,7 +140,7 @@ function likeButtonRender() {
 }
 
 function initializeSong() {
-  cover.src = `images/${sortedPlaylist[index].file}.webp`;
+  cover.src = `images/${sortedPlaylist[index].file}.jpg`;
   song.src = `songs/${sortedPlaylist[index].file}.mp3`;
   songName.innerText = sortedPlaylist[index].songName;
   bandName.innerText = sortedPlaylist[index].artist;
